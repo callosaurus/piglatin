bea93a21f70bc0b72073281cd29e3fc593d63500
callosaurus <callumdavies421@gmail.com>
2017-02-10 18:05:35 -0800

    first commit

---
+++ Pig Latin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pig Latin
+//
+//  Created by Callum Davies on 2017-02-10.
+//  Copyright © 2017 Callum Davies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
